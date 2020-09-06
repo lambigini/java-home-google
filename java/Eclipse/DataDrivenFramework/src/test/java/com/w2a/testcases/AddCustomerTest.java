582f61e39c1791e57a0acea3bf854cc9e85a220d
Harrison <harrisontran@outlook.com>
2020-09-06 09:09:11 -0700

    sep 6 2020
    revert to sept 5, and reorginize folder because problem with eclipse

--- java/Eclipse/DataDrivenFramework/src/test/java/com/w2a/testcases/AddCustomerTest.java
+++ java/Eclipse/DataDrivenFramework/src/test/java/com/w2a/testcases/AddCustomerTest.java
@@ -1,3 +1,4 @@
+
 package com.w2a.testcases;
 
 
@@ -17,24 +18,22 @@ import com.w2a.utilities.TestUtil;
 public class AddCustomerTest extends TestBase {
 
 	@Test (dataProviderClass = TestUtil.class ,dataProvider = "dp")
-	public void addCustomer(String firstName, String lastName, String postalCode, String alertText) throws InterruptedException {
-		click("bmlBtn_CSS");
+	public void addCustomer(String firstName, String lastName, String postalCode, String alertText) throws InterruptedException {		click("bmlBtn_CSS");
 		click("addCustBtn_CSS");
 		type("firstname_CSS", firstName);
 		type("lastname_XPATH", lastName);
 		type("postcode_CSS", postalCode);
 		click("addbtn_CSS");
-		
+		Thread.sleep(2000);
 		wait = new WebDriverWait(driver, 5);
 		
 	Alert alert =	wait.until(ExpectedConditions.alertIsPresent());
 	Assert.assertTrue(alert.getText().contains(alertText));
-	
-	Thread.sleep(2000);
+	//driver.switchTo().alert().accept();
 	alert.accept();
+
 	Thread.sleep(2000);
 	
-	Assert.fail("add customer not succeful");
 	}
 	
 	
