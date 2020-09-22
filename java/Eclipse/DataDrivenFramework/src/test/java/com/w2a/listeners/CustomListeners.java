3e1f5e1f68f7ba96eec3df961b305a8134585c2b
Harrison <harrisontran@outlook.com>
2020-09-21 18:32:12 -0700

    sep 21

--- java/Eclipse/DataDrivenFramework/src/test/java/com/w2a/listeners/CustomListeners.java
+++ java/Eclipse/DataDrivenFramework/src/test/java/com/w2a/listeners/CustomListeners.java
@@ -1,6 +1,11 @@
 package com.w2a.listeners;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
 
 import org.testng.ITestContext;
 import org.testng.ITestListener;
@@ -10,10 +15,14 @@ import org.testng.SkipException;
 
 import com.relevantcodes.extentreports.LogStatus;
 import com.w2a.base.TestBase;
+import com.w2a.utilities.MonitoringMail;
+import com.w2a.utilities.TestConfig;
 import com.w2a.utilities.TestUtil;
 
 public class CustomListeners extends TestBase implements ITestListener {
 
+	public String messageBody;
+	
 	public void onTestStart(ITestResult result) {
 	test =rep.startTest(result.getName().toUpperCase());
 	System.out.println(result.getName().toUpperCase() + " start");
@@ -68,7 +77,25 @@ public class CustomListeners extends TestBase implements ITestListener {
 	}
 
 	public void onStart(ITestContext context) {
-		// TODO Auto-generated method stub
+		MonitoringMail mail =  new MonitoringMail();
+		
+		try {
+			messageBody = "http://" + 	InetAddress.getLocalHost().getHostAddress() +":8080/job/DataDrivenLiveProject/Extent_20HTML_20Report/";
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		try {
+			mail.sendMail(TestConfig.server, TestConfig.from, TestConfig.to, TestConfig.subject, messageBody);
+		} catch (AddressException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
